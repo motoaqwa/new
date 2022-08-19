338458e623e845a72f6b2ad61731bca6bcce48f7
motoaqwa <111219664+motoaqwa@users.noreply.github.com>
2022-08-19 13:27:09 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("A B C D E F G H");
+Console.WriteLine("---------------");
+for (int i = 8; i > 0; i--)
+{
+    for (int j = 1; j < 10; j++)
+    {
+        if (j == 9)
+        {
+            Console.Write("|" + i);
+        }
+        else
+        {
+            if ((i + j) % 2 == 0)
+            {
+                Console.Write('#' + " ");
+            }
+            else
+            {
+                Console.Write(((char)79).ToString() + " ");
+            }
+        }
+    }
+    Console.WriteLine();
+
+}
+Console.WriteLine();
