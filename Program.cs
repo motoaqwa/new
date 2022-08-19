84decb05659315ca8f17e799b1e280691fe75db4
motoaqwa <111219664+motoaqwa@users.noreply.github.com>
2022-08-19 13:27:56 +0300

    Update Program.cs
    MaxOfThree

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, users!!! \n A simple task is to enter three numbers and find the largest possible value\n Enter numer: ");
+
+int userInput1;
+userInput1 = Convert.ToInt32(Console.ReadLine());
+int userInput2;
+Console.WriteLine($"Enter the second number: ");
+userInput2 = Convert.ToInt32(Console.ReadLine());
+int userInput3;
+Console.WriteLine($"Enter the third number: ");
+userInput3 = Convert.ToInt32(Console.ReadLine());
+
+if (userInput1 > userInput2 && userInput1 > userInput3)
+    Console.WriteLine($"maximum number : {userInput1}");
+
+else if (userInput2 > userInput1 && userInput2 > userInput3)
+    Console.WriteLine($"maximum number : {userInput2}");
+
+else if (userInput3 > userInput1 && userInput3 > userInput2)
+    Console.WriteLine($"maximum number : {userInput3}");
