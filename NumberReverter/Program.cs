6b14a565202b09a9821a840a8e8ac982d1ad7eaf
motoaqwa <111219664+motoaqwa@users.noreply.github.com>
2022-08-23 17:12:04 +0300

    not all

--- NumberReverter/Program.cs
+++ NumberReverter/Program.cs
@@ -1,14 +1,27 @@
 ﻿Console.WriteLine("Enter number: ");
-
-// TODO: Програма не працює для великих чисел, наприклад 3147483647
-// Підказка: почитати, що таке Int32, яке найбільше значення він має. Які ще є типи
 uint num = Convert.ToUInt32(Console.ReadLine());
+
 string result = "";
-int a = num % 10;
+uint a = num % 10;
 num = num / 10;
 result += a;
 
-// TODO: Цей код повторюється 5 разів, подумай, як усунути цю проблему
+a = num % 10;
+num = num / 10;
+result += a;
+//як змінити повторення в голову не приходить
+a = num % 10;
+num = num / 10;
+result += a ;
+
+a = num % 10;
+num = num / 10;
+result += a;
+
+a = num % 10;
+num = num / 10;
+result += a;
+
 a = num % 10;
 num = num / 10;
 result += a;
