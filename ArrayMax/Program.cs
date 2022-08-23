d8b11c0f0034bfddcee314548c624a87fbb29b90
motoaqwa <111219664+motoaqwa@users.noreply.github.com>
2022-08-23 14:11:10 +0300

    MaxOfSix

--- ArrayMax/Program.cs
+++ ArrayMax/Program.cs
@@ -1,6 +1,39 @@
-﻿// Користувач вводить декілька чисел через кому, зробити з них масив та вивести у консоль найбільше число.
-//     Наприклад, користувач вводить
-// 1, 2, 5, 6, 7, 9
-// Вивести - 9
-
-Console.WriteLine("Додай своє рішення проблеми");
+﻿        Console.WriteLine("enter number:");
+        int[] array;
+        array = new int[6];
+        array[0] = Convert.ToInt32(Console.ReadLine());
+        array[1] = Convert.ToInt32(Console.ReadLine());
+        array[2] = Convert.ToInt32(Console.ReadLine());
+        array[3] = Convert.ToInt32(Console.ReadLine());
+        array[4] = Convert.ToInt32(Console.ReadLine());
+        array[5] = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < array.Length; i++) ;
+        {
+            Console.WriteLine($"your numbers look like this:" + (array[0],+ array[1],+ array[2],+ array[3],+ array[4],+ array[5]));
+    }
+            if(array[0]> array[1]&& array[0]> array[2]&& array[0]> array[3] && array[0] > array[4] && array[0] > array[5])
+            {
+                Console.WriteLine("max of number =" +array[0]);
+            }
+            else if (array[1] > array[0] && array[1] > array[2] && array[1] > array[3] && array[1] > array[4] && array[1] > array[5])
+            {
+                Console.WriteLine("max of number =" +array[1]);
+            }
+            else if(array[2] > array[0] && array[2] > array[1] && array[2] > array[3] && array[2] > array[4] && array[2] > array[5])
+            {
+                Console.WriteLine("max of number =" +array[2]);
+            }
+            else if(array[3] > array[0] && array[3] > array[1] && array[3] > array[2] && array[3] > array[4] && array[3] > array[5])
+            {
+                Console.WriteLine("max of number =" +array[3]);
+            }
+            else if (array[4] > array[0] && array[4] > array[1] && array[4] > array[1] && array[4] > array[3] && array[4] > array[5])
+            {
+                Console.WriteLine("max of number =" +array[0]);
+            }
+            else if (array[5] > array[0] && array[5] > array[1] && array[5] > array[2] && array[5] > array[3] && array[5] > array[4])
+            {
+                Console.WriteLine("max of number =" +array[5 ]);
+            }
+            Console.ReadLine();
+            
--- NumberReverter/Program.cs
+++ NumberReverter/Program.cs
@@ -2,29 +2,13 @@
 
 // TODO: Програма не працює для великих чисел, наприклад 3147483647
 // Підказка: почитати, що таке Int32, яке найбільше значення він має. Які ще є типи
-int num = Convert.ToInt32(Console.ReadLine());
+uint num = Convert.ToUInt32(Console.ReadLine());
 string result = "";
 int a = num % 10;
 num = num / 10;
 result += a;
 
 // TODO: Цей код повторюється 5 разів, подумай, як усунути цю проблему
-a = num % 10;
-num = num / 10;
-result += a;
-
-a = num % 10;
-num = num / 10;
-result += a ;
-
-a = num % 10;
-num = num / 10;
-result += a;
-
-a = num % 10;
-num = num / 10;
-result += a;
-
 a = num % 10;
 num = num / 10;
 result += a;
