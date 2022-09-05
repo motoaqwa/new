21c8300e29d93a45c1bcfc64c04961580cfbfaeb
Artem Horbunov <Artem_Horbunov1@epam.com>
2022-09-05 15:06:24 +0300

    додав рішення через строку

--- NumberReverter/Program.cs
+++ NumberReverter/Program.cs
@@ -1,29 +1,7 @@
 ﻿Console.WriteLine("Enter number: ");
-uint num = Convert.ToUInt32(Console.ReadLine());
+string num = Console.ReadLine();
 
-string result = "";
-uint a = num % 10;
-num = num / 10;
-result += a;
-
-a = num % 10;
-num = num / 10;
-result += a;
-//як змінити повторення в голову не приходить
-a = num % 10;
-num = num / 10;
-result += a ;
-
-a = num % 10;
-num = num / 10;
-result += a;
-
-a = num % 10;
-num = num / 10;
-result += a;
-
-a = num % 10;
-num = num / 10;
-result += a;
-
-Console.WriteLine(result);
+for (int i = num.Length - 1; i >= 0; i--)
+{
+    Console.Write(num[i]);
+}
